a1a2dccfd75540d5f54cfd1f1ccb4522f967218f
mulchus <mulch@list.ru>
2024-01-15 23:41:22 +0300

    add request_smsc function.

--- smsc_api.py
+++ smsc_api.py
@@ -3,7 +3,14 @@ import asyncclick as click
 
 from environs import Env
 from contextlib import suppress
+from contextvars import ContextVar
 
+
+class SmscApiError(Exception):
+    def __init__(self, message, extra_info=None):
+        super().__init__(message)
+        # self.extra_info = extra_info
+        
 
 @click.command()
 @click.option(
@@ -73,6 +80,92 @@ async def main(login, psw, valid, phones, msg):
     response = await asks.post(status_url, json=params)
     print(response.json())
     # print(response.text)
+
+
+smsc_login: ContextVar[str] = ContextVar('smsc_login')
+smsc_password: ContextVar[str] = ContextVar('smsc_password')
+
+
+async def request_smsc(
+        http_method: str,
+        api_method: str,
+        *,
+        login: str,
+        password: str,
+        payload: dict = {}
+) -> dict:
+    """Send request to SMSC.ru service.
+
+    Args:
+        http_method (str): E.g. 'GET' or 'POST'.
+        api_method (str): E.g. 'send' or 'status'.
+        login (str): Login for account on smsc.ru.
+        password (str): Password for account on smsc.ru.
+        payload (dict): Additional request params, override default ones.
+    Returns:
+        dict: Response from smsc.ru API.
+    Raises:
+        SmscApiError: If smsc.ru API response status is not 200 or JSON response
+        has "error_code" inside.
+
+    Examples:
+        >>> await request_smsc(
+        ...   'POST',
+        ...   'send',
+        ...   login='smsc_login',
+        ...   password='smsc_password',
+        ...   payload={'phones': '+79123456789'}
+        ... )
+        {'cnt': 1, 'id': 24}
+        >>> await request_smsc(
+        ...   'POST',
+        ...   'status',
+        ...   login='smsc_login',
+        ...   password='smsc_password',
+        ...   payload={
+        ...     'phone': '+79123456789',
+        ...     'id': '24',
+        ...   }
+        ... )
+        {'status': 1, 'last_date': '28.12.2019 19:20:22', 'last_timestamp': 1577550022}
+    """
+    # raise NotImplementedError
+    
+    payload['login'] = login
+    payload['psw'] = password
+    payload['fmt'] = 3
+
+    if http_method == 'POST' and api_method == 'send':
+        payload['op'] = 1
+        payload['err'] = 1
+        payload['all'] = 2
+        payload['cost'] = 1
+        payload['mes'] = 'Внимание, вечером будет шторм!'
+        
+        # print(payload)
+        response = await asks.post(
+            'https://smsc.ru/rest/send/',
+            json=payload,
+        )
+        print(response.json())
+        
+        if 'error_code' in response.json():
+            raise SmscApiError(f'API error {response.json()}')
+
+    elif http_method == 'POST' and api_method == 'status':
+        
+        print(payload)
+        response = await asks.post(
+            'https://smsc.ru/rest/status/',
+            json=payload,
+        )
+        print(response.json())
+        
+        if 'error_code' in response.json():
+            raise SmscApiError(f'API error {response.json()}')
+        
+    else:
+        raise SmscApiError(f'API error, http_method: {http_method}, api_method: {api_method}')
 
 
 if __name__ == '__main__':
