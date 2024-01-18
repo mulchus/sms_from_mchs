b615d836aa9e51a77367f21aecaa36b5ffafc6c1
mulchus <mulch@list.ru>
2024-01-18 23:08:06 +0300

    Upd to Trio nursery del queue

--- server.py
+++ server.py
@@ -1,32 +1,9 @@
-import asyncio
 import trio
 
-from typing import AsyncGenerator
-from quart import Quart, render_template, websocket
+from quart import render_template, websocket
 from quart_trio import QuartTrio
 
-
-class Broker:
-    def __init__(self) -> None:
-        self.connections = set()
-
-    async def publish(self, message: str) -> None:
-        for connection in self.connections:
-            await connection.put(message)
-
-    async def subscribe(self) -> AsyncGenerator[str, None]:
-        connection = asyncio.Queue()
-        self.connections.add(connection)
-        try:
-            while True:
-                yield await connection.get()
-        finally:
-            self.connections.remove(connection)
-
-
-app = Quart(__name__)
-# app = QuartTrio(__name__)
-broker = Broker()
+app = QuartTrio(__name__)
 
 
 @app.get("/")
@@ -34,28 +11,20 @@ async def index():
     return await render_template("index.html")
 
 
-async def _receive() -> None:
+async def receive():
     while True:
-        message = await websocket.receive()
-        await broker.publish(f'Received: echo {message}')
+        await websocket.send(f'Received: {await websocket.receive()}')
 
 
-@app.websocket("/ws")
-async def ws() -> None:
-    async with asyncio.TaskGroup() as tg:
-        tg.create_task(_receive())
-        async for message in broker.subscribe():
-            await websocket.send(message)
-    
-    # try:
-    #     async with trio.open_nursery() as nursery:
-    #         nursery.start_soon(_receive)
-    #         async for message in broker.subscribe():
-    #             await websocket.send(message)
-    
-    # except BaseException as e:
-    #     print(f'websocket funcs crashed with exception: {e}')
+@app.websocket('/ws')
+async def ws():
+    try:
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(receive)
+   
+    except BaseException as e:
+        print(f'websocket funcs crashed with exception: {e}')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
